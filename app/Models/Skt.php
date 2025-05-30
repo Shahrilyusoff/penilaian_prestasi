ca2e461c61545d79b9a14ad006edca1da373c541
shahrilyusoff <2021120453@student.uitm.edu.my>
2025-05-30 10:38:51 +0800

    Skt flow

--- app/Models/Skt.php
+++ app/Models/Skt.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Skt extends Model
 {
     use HasFactory;
-    protected $table = 'skt';
+    protected $table ='skt';
 
     protected $fillable = [
         'evaluation_period_id',
